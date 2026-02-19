9daaab4b3b6b900600feef38707525ea3ed81793
Tuna <takaanume@gmail.com>
2026-02-19 10:35:15 +0500

    Add more functions and comprehensive tests

--- math_tools.py
+++ math_tools.py
@@ -8,3 +8,38 @@ def multiply(a, b):
 
 def is_even(n):
     return n % 2 == 0
+
+def subtract(a,b):
+    return a-b
+
+def max_of_three(a,b,c):
+    return max(a,b,c)
+
+def is_palindrome(s):
+    ans = ''
+    for i in s:
+        if i != ' ':
+            ans += i
+    if ans == ans[::-1]:
+        return True
+    else:
+        return False
+
+def find_min(numbers):
+    min_num = 0  #just for now
+    for i in range(len(numbers)):
+        if i==0:
+            min_num = numbers[i]
+        elif numbers[i]<= min_num:
+                min_num = numbers[i]
+
+    return min_num
+
+def remove_duplicates(items):
+    newlist = []
+    for i in items:
+        if i not in newlist:
+            newlist.append(i)
+    return newlist
+
+        
--- test_math_tools.py
+++ test_math_tools.py
@@ -1,6 +1,6 @@
 #checker mathtools 
 
-from math_tools import add, multiply, is_even
+from math_tools import add, multiply, is_even, subtract, max_of_three, is_palindrome, find_min, remove_duplicates
 
 def test_add():
     assert add(2, 3) == 5
@@ -20,8 +20,52 @@ def test_is_even():
     assert is_even(0) == True
     print("test_is_even: ALL PASSED")
 
+def test_subtract():
+    assert subtract(2,4) == -2   #minus ans
+    assert subtract(0,-1) == 1  #sign change 
+    assert subtract(796,3) == 793
+    print("test_subtract: ALL PASSED")
+
+def test_max_of_three():
+    assert max_of_three(2,2,5) == 5
+    assert max_of_three(3,3,3) == 3
+    assert max_of_three(-4, 0,-9) == 0
+    assert max_of_three(54.6,6,9) == 54.6
+    assert max_of_three(-5,-5,9) == 9
+    print("test_max_of_three: ALL PASSED")
+
+def test_is_palindrome():
+    assert is_palindrome('hello') == False
+    assert is_palindrome('racecar') == True
+    assert is_palindrome('racecar mom civic') == False
+    assert is_palindrome('Racecar mom civic') == False
+    assert is_palindrome('') == True
+    assert is_palindrome("RaCeCAR") == False
+    print("test_is_palindrome: ALL PASSED")
+
+def test_find_min():
+    assert find_min([0,23,-3]) == -3
+    assert find_min([4,2,4,7,4,1]) == 1
+    assert find_min([-2,-5,2]) == -5
+    print("test_find_min: ALL PASSED")
+
+def test_remove_duplicates():
+    assert remove_duplicates(['s','a','s','a','t','c','x']) == ['s','a','t','c','x']
+    assert remove_duplicates([1,2,2,3]) == [1,2,3]
+    assert remove_duplicates([3,1,2,1]) == [3,1,2]
+    assert remove_duplicates([5,5,5]) == [5]
+    assert remove_duplicates([]) == []
+    assert remove_duplicates(["a","b","a"]) == ["a","b"]
+    print("test_remove_duplicates = ALL PASSED")
+
+
 # Run all tests
 test_add()
 test_multiply()
 test_is_even()
+test_subtract()
+test_max_of_three()
+test_is_palindrome()
+test_find_min()
+test_remove_duplicates()
 print("--- All tests passed! ---")
